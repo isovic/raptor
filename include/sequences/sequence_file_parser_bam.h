88d666c32a0ff62c4e16c701a2d3ef56960798f8
Ivan Sovic <ivan.sovic@gmail.com>
2019-05-06 12:04:12 +0200

    Discovered a bug in PacBio::BAM::SamTagCodec::Encode, which encodes a float as 'i' in the tag. This ends up breaking Pbindex which looks for rq:f: but finds rq:i:, and also the float value is rounded down to the int. This change fixes that by copying the encoder into src/sequences/sequence_file_parser_bam.cc and fixing the bug, thus it's not dependant on the Pbbam version.

--- include/sequences/sequence_file_parser_bam.h
+++ include/sequences/sequence_file_parser_bam.h
@@ -49,6 +49,7 @@ private:
 
    void ParseHeader_();
    void ParseReadGroupAndProgramGroupFromHeader_(const std::string& header);
+   std::string EncodeSamTags_(const PacBio::BAM::TagCollection& tags) const;
 
    std::string path_;
    std::unique_ptr<PacBio::BAM::BamReader> bam_reader_;
