bdcd19ba54d6fed4649a4afbb89827e44564c012
Ivan Sovic <ivan.sovic@gmail.com>
2019-07-10 13:39:10 +0200

    Minor cleanup in src/raptor_fetch/args_raptor_fetch.cc.

--- src/raptor_fetch/args_raptor_fetch.cc
+++ src/raptor_fetch/args_raptor_fetch.cc
@@ -33,7 +33,11 @@ int ProcessArgsRaptorFetch(int argc, char **argv, std::shared_ptr<raptor::Params
                           "Prefix of the output files to generate.", 0,
                           "Input/Output options");
     argparser.AddArgument(&params->job_str, VALUE_TYPE_STRING, "", "job", "erc",
-                          "Either: clip, apply, noncontained, erc, fetch", 0,
+                          "Options:"
+                          "\n fetch - The input is a list of qnames, one per line. Fetches the sequences from the rdb."
+                          "\n clip  - The input is a BED-3 file of sequences and regions to extract."
+                          "\n erc   - The input is an M4 file of overlaps. Output is a pile of sequences for Falcon consensus."
+                          , 0,
                           "Input/Output options");
     argparser.AddArgument(&params->min_cov, VALUE_TYPE_INT32, "", "min-cov", "0",
                           "Minimum coverage of a sequence to retain it.",
@@ -113,72 +117,5 @@ int ProcessArgsRaptorFetch(int argc, char **argv, std::shared_ptr<raptor::Params
 
     return 0;
 }
-
-// int ProcessArgsRaptorFetch1(int argc, char **argv, std::shared_ptr<raptor::ParamsRaptorFetch> parameters) {
-//     bool help = false;
-//     bool version = false;
-
-//     ArgumentParser argparser;
-
-//     argparser.AddArgument(&parameters->in_path, VALUE_TYPE_STRING, "i", "input", "",
-//                           "Input Raptor DB file.", 0,
-//                           "Input/Output options");
-
-//     argparser.AddArgument(&version, VALUE_TYPE_BOOL, "", "version", "0", "Output the version and exit.", 0,
-//                           "Other options");
-
-//     argparser.AddArgument(&help, VALUE_TYPE_BOOL, "h", "help", "0", "View this help.", 0,
-//                           "Other options");
-
-//     argparser.ProcessArguments(argc, argv);
-
-//     // Store the command line in parameters.
-//     std::stringstream ss_command_line;
-//     for (int i = 0; i < argc; i++) {
-//         if (i > 0) ss_command_line << " ";
-//         ss_command_line << argv[i];
-//     }
-//     parameters->command_line = ss_command_line.str();
-
-//     // Check if help was triggered.
-//     if (argparser.GetArgumentByLongName("help")->is_set == true) {
-//         std::stringstream ss;
-//         ss << SOFTWARE_NAME
-//            << " - Data fetching tool for Raptor - A very accurate and sensitive long-read, high error-rate sequence mapper\n"
-//            << SOFTWARE_NAME << " ";
-//         ss << "Version: " << RAPTOR_VERSION_STRING << "\n";
-//         ss << "Build date: " << std::string(RELEASE_DATE).c_str() << "\n";
-//         ss << "\n";
-//         ss << LICENCE_INFORMATION << "\n";
-//         ss << "\n";
-//         ss << "Usage:\n";
-//         ss << "\traptor-reshape [options] -r <reference_file>";
-//         ss << "\n";
-
-//         fprintf(stderr, "%s\n", ss.str().c_str());
-//         fprintf(stderr, "%s\n", argparser.VerboseUsage().c_str());
-//         exit(0);
-//     }
-
-//     if (version) {
-//         std::stringstream ss;
-//         ss << RAPTOR_VERSION_STRING << "\n";
-//         fprintf(stdout, "%s", ss.str().c_str());
-//         exit(0);
-//     }
-
-//     // Sanity check for the input path.
-//     if (!raptor::FileExists(parameters->in_path.c_str())) {
-//         fprintf(stderr, "Input file does not exist: '%s'\n\n", parameters->in_path.c_str());
-//         VerboseShortHelpAndExit(argc, argv);
-//     }
-
-//     // Write this out for every debug verbose level.
-//     if (parameters->verbose_level > 5) {
-//         fprintf(stderr, "%s\n", argparser.VerboseArguments().c_str());
-//     }
-
-//     return 0;
-// }
 
 } /* namespace raptor */
