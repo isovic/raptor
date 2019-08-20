d1b50f69851d4600543c8f0a92e909b0761fcbd8
Ivan Sovic <ivan.sovic@gmail.com>
2019-08-20 12:25:14 +0200

    Added one more test for ScoreCigarAlignment.

--- tests/unit/test_cigar.cc
+++ tests/unit/test_cigar.cc
@@ -33,3 +33,13 @@ TEST(Cigar, ScoreCigarAlignment4) {
     int64_t expected = 20 + (-2 - 4 * 4) + (-2 - 6 * 4) - 8 + 10;
     ASSERT_EQ(result, expected);
 }
+
+TEST(Cigar, ScoreCigarAlignment5) {
+    // Only extended CIGAR operations are defined: '=', 'X', 'I', 'D'.
+    // This tests occurrences of other characters, which will simply be ignored.
+    std::vector<raptor::CigarOp> cigar = { {'M', 10}, {'I', 5}, {'D', 7}, {'X', 2}, {'N', 5} };
+    int32_t match = 2, mismatch = -4, gap_open = -2, gap_ext = -4;
+    int64_t result = raptor::ScoreCigarAlignment(cigar, match, mismatch, gap_open, gap_ext);
+    int64_t expected = 0 + (-2 - 4 * 4) + (-2 - 6 * 4) - 8 + 0;
+    ASSERT_EQ(result, expected);
+}
