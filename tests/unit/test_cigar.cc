4b75e32e6b6e9a3eb4b4e020581280f17a47d80f
Ivan Sovic <ivan.sovic@gmail.com>
2019-08-20 12:12:49 +0200

    Added unit tsts for the new ScoreCigarAlignment.

---
+++ tests/unit/test_cigar.cc
@@ -0,0 +1,35 @@
+#include <gtest/gtest.h>
+
+#include <aligner/cigar.h>
+
+TEST(Cigar, ScoreCigarAlignment1) {
+    std::vector<raptor::CigarOp> cigar = {};
+    int32_t match = 1, mismatch = 1, gap_open = 1, gap_ext = 1;
+    int64_t result = raptor::ScoreCigarAlignment(cigar, match, mismatch, gap_open, gap_ext);
+    int64_t expected = 0;
+    ASSERT_EQ(result, expected);
+}
+
+TEST(Cigar, ScoreCigarAlignment2) {
+    std::vector<raptor::CigarOp> cigar = { {'=', 10} };
+    int32_t match = 1, mismatch = 1, gap_open = 1, gap_ext = 1;
+    int64_t result = raptor::ScoreCigarAlignment(cigar, match, mismatch, gap_open, gap_ext);
+    int64_t expected = 10;
+    ASSERT_EQ(result, expected);
+}
+
+TEST(Cigar, ScoreCigarAlignment3) {
+    std::vector<raptor::CigarOp> cigar = { {'=', 10}, {'I', 1}, {'X', 2}, {'D', 1}, {'=', 5} };
+    int32_t match = 2, mismatch = -4, gap_open = -2, gap_ext = -4;
+    int64_t result = raptor::ScoreCigarAlignment(cigar, match, mismatch, gap_open, gap_ext);
+    int64_t expected = 20 - 2 - 8 - 2 + 10;
+    ASSERT_EQ(result, expected);
+}
+
+TEST(Cigar, ScoreCigarAlignment4) {
+    std::vector<raptor::CigarOp> cigar = { {'=', 10}, {'I', 5}, {'D', 7}, {'X', 2}, {'=', 5} };
+    int32_t match = 2, mismatch = -4, gap_open = -2, gap_ext = -4;
+    int64_t result = raptor::ScoreCigarAlignment(cigar, match, mismatch, gap_open, gap_ext);
+    int64_t expected = 20 + (-2 - 4 * 4) + (-2 - 6 * 4) - 8 + 10;
+    ASSERT_EQ(result, expected);
+}
