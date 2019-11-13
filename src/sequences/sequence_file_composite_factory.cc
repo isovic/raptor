0efa8e282f2e8936b26b8084d59641fab521bba3
Ivan Sovic <ivan.sovic@gmail.com>
2019-11-13 12:32:40 +0100

    Cleanup in src/sequences/sequence_file_composite_factory.cc.

--- src/sequences/sequence_file_composite_factory.cc
+++ src/sequences/sequence_file_composite_factory.cc
@@ -7,41 +7,31 @@
 
 #include <sequences/sequence_file_composite_factory.h>
 #include <sequences/sequence_file_enums.h>
+#include <sequences/sequence_file_utils.h>
 #include <utility/stringutil.h>
 #include <utility/files.hpp>
 
 namespace mindex {
 
 mindex::SequenceFileCompositeBasePtr createSequenceFileCompositeFactory(const std::vector<std::string>& in_paths, mindex::SequenceFormat in_fmt) {
+	// This checks that there is at most one RaptorDB or one XML file specified,
+	// and that all input files actually exist on disk.
+	bool rv = mindex::ValidateInputFiles(in_fmt, in_paths);
 
-	bool is_xml_used = false;
-
-#ifdef RAPTOR_COMPILED_WITH_PBBAM
-	// Sanity check for the input files. Allow only one XML file to be specified,
-	// as by design of the SequenceFileCompositePbXml.
-	for (const auto& in_path: in_paths) {
-		if (mindex::GetSequenceFormatFromPath(in_path) == mindex::SequenceFormat::XML) {
-			is_xml_used = true;
-			break;
-		}
+	if (rv == false) {
+		FATAL_REPORT(ERR_UNEXPECTED_VALUE, "Invalid input files.");
 	}
-#endif
 
 	mindex::SequenceFileCompositeBasePtr seq_file_parser = nullptr;
 
-	// Set-up the parser for the correct sequence format.
-	if (is_xml_used && in_paths.size() != 1) {
-		FATAL_REPORT(ERR_UNEXPECTED_VALUE, "When using XML as input, only a single input can be specified.");
-
-	} else if (is_xml_used && in_paths.size() == 1) {
-        #ifdef RAPTOR_COMPILED_WITH_PBBAM
-                seq_file_parser = mindex::createSequenceFileCompositePbXml(in_paths[0]);
-        #else
-                FATAL_REPORT(ERR_UNEXPECTED_VALUE, "The raptor-reshape tool was not compiled with PacBio BAM support. Cannot use the .xml input files.");
-        #endif
-
-	} else {
-
+	bool is_xml_used = false;
+	#ifdef RAPTOR_COMPILED_WITH_PBBAM
+		if (in_fmt == mindex::SequenceFormat::XML) {
+				is_xml_used = true;
+				seq_file_parser = mindex::createSequenceFileCompositePbXml(in_paths[0]);
+		}
+	#endif
+	if (is_xml_used == false) {
 		seq_file_parser = mindex::createSequenceFileCompositeFofn(in_paths, in_fmt);
 	}
 
