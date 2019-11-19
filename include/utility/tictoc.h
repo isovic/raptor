4d21b235e54c41c92ca984b10bd6b499fce3b2f3
Ivan Sovic <ivan.sovic@gmail.com>
2019-11-19 15:56:49 +0100

    Extended TicToc to measure CPU time as well.

--- include/utility/tictoc.h
+++ include/utility/tictoc.h
@@ -8,7 +8,7 @@
 #ifndef SRC_CONSENSUS_TicToc2_H_
 #define SRC_CONSENSUS_TicToc2_H_
 
-#include <time.h>
+#include <ctime>
 #include <chrono>
 
 class TicToc {
@@ -18,14 +18,23 @@ class TicToc {
 
     void start();
     void stop();
-    double get_secs(bool current = false);
-    double get_millisecs(bool current = false);
-    double get_microsecs(bool current = false);
-    double get_nanosecs(bool current = false);
+    double get_secs(bool current = false) const;
+    double get_millisecs(bool current = false) const;
+    double get_microsecs(bool current = false) const;
+    double get_nanosecs(bool current = false) const;
+
+    double get_cpu_secs(bool current = false) const;
+    double get_cpu_millisecs(bool current = false) const;
+    double get_cpu_microsecs(bool current = false) const;
+    double get_cpu_nanosecs(bool current = false) const;
 
   private:
     std::chrono::time_point<std::chrono::high_resolution_clock> start_;
     std::chrono::time_point<std::chrono::high_resolution_clock> end_;
+    std::clock_t start_cpu_;
+    std::clock_t end_cpu_;
+
+    inline double get_cpu_duration_(bool current, double factor) const;
 };
 
 #endif /* SRC_CONSENSUS_TicToc_H_ */
--- src/utility/tictoc.cc
+++ src/utility/tictoc.cc
@@ -19,32 +19,56 @@ TicToc::~TicToc() {
 
 void TicToc::start() {
     start_ = std::chrono::high_resolution_clock::now();
+    start_cpu_ = std::clock();
 }
 
 void TicToc::stop() {
     end_ = std::chrono::high_resolution_clock::now();
+    end_cpu_ = std::clock();
 }
 
-double TicToc::get_secs(bool current) {
+double TicToc::get_secs(bool current) const {
     auto end = (current) ? std::chrono::high_resolution_clock::now() : end_;
     double elapsed = std::chrono::duration_cast<std::chrono::seconds>(end - start_).count();
     return elapsed;
 }
 
-double TicToc::get_millisecs(bool current) {
+double TicToc::get_millisecs(bool current) const {
     auto end = (current) ? std::chrono::high_resolution_clock::now() : end_;
     double elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start_).count();
     return elapsed;
 }
 
-double TicToc::get_microsecs(bool current) {
+double TicToc::get_microsecs(bool current) const {
     auto end = (current) ? std::chrono::high_resolution_clock::now() : end_;
     double elapsed = std::chrono::duration_cast<std::chrono::microseconds>(end - start_).count();
     return elapsed;
 }
 
-double TicToc::get_nanosecs(bool current) {
+double TicToc::get_nanosecs(bool current) const {
     auto end = (current) ? std::chrono::high_resolution_clock::now() : end_;
     double elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start_).count();
     return elapsed;
+}
+
+double TicToc::get_cpu_duration_(bool current, double factor) const {
+    auto end_cpu = (current) ? std::clock() : end_cpu_;
+    double elapsed = factor * static_cast<double>(end_cpu - start_cpu_) / CLOCKS_PER_SEC;
+    return elapsed;
+}
+
+double TicToc::get_cpu_secs(bool current) const {
+    return get_cpu_duration_(current, 1.0);
+}
+
+double TicToc::get_cpu_millisecs(bool current) const {
+    return get_cpu_duration_(current, 1000.0);
+}
+
+double TicToc::get_cpu_microsecs(bool current) const {
+    return get_cpu_duration_(current, 1000000.0);
+}
+
+double TicToc::get_cpu_nanosecs(bool current) const {
+    return get_cpu_duration_(current, 1000000000.0);
 }
