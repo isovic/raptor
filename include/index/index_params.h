53742e5fa2688ef330261e626b5c13b6ca152788
Ivan Sovic <ivan.sovic@gmail.com>
2019-11-20 13:57:25 +0100

    Updated index_params, now it's systematic with params_mapper. All defaults are set in the header, and the cpp is removed.

--- include/index/index_params.h
+++ include/index/index_params.h
@@ -15,44 +15,48 @@
 
 namespace mindex {
 
-class IndexParams;
-
-std::shared_ptr<mindex::IndexParams> createIndexParams();
-std::shared_ptr<mindex::IndexParams> createIndexParams(int32_t _k, int32_t _w, bool _hp_supp,
-                                                       int32_t _max_hp_len, double _freq_percentile,
-                                                       int32_t _min_occ_cutoff,
-                                                       bool _index_only_fwd_strand,
-                                                       int64_t min_tlen);
-
 class IndexParams {
    public:
     friend std::shared_ptr<mindex::IndexParams> createIndexParams();
     friend std::shared_ptr<mindex::IndexParams> createIndexParams(
         int32_t _k, int32_t _w, bool _hp_supp, int32_t _max_hp_len, double _freq_percentile,
         int32_t _min_occ_cutoff, bool _index_only_fwd_strand, int64_t _min_tlen);
-    ~IndexParams();
+    ~IndexParams() = default;
 
-    int32_t k;
-    int32_t w;
-    bool homopolymer_suppression;
-    int32_t max_homopolymer_len;
-    double freq_percentil;
-    int32_t min_occ_cutoff;
-
-    bool is_region_specified;
+    int32_t k = 15;
+    int32_t w = 5;
+    bool homopolymer_suppression = false;
+    int32_t max_homopolymer_len = 10;
+    double freq_percentil = 0.002;
+    int32_t min_occ_cutoff = -1;
+    bool is_region_specified = false;
     std::string region_rname;
-    int64_t region_rstart;
-    int64_t region_rend;
-
-    bool index_only_fwd_strand;
-
-    int64_t min_tlen;
+    int64_t region_rstart = 0;
+    int64_t region_rend = -1;
+    bool index_only_fwd_strand = false;
+    int64_t min_tlen = 0;
 
    private:
-    IndexParams();
+    IndexParams() = default;
     IndexParams(int32_t _k, int32_t _w, bool _hp_supp, int32_t _max_hp_len, double _freq_percentile,
                 int32_t _min_occ_cutoff, bool _index_only_fwd_strand, int64_t _min_tlen);
+    IndexParams(const IndexParams&) = delete;
+    IndexParams& operator=(const IndexParams&) = delete;
 };
+
+inline std::shared_ptr<mindex::IndexParams> createIndexParams() {
+    return std::shared_ptr<mindex::IndexParams>(new mindex::IndexParams());
+}
+
+inline std::shared_ptr<mindex::IndexParams> createIndexParams(int32_t _k, int32_t _w, bool _hp_supp,
+                                                       int32_t _max_hp_len, double _freq_percentile,
+                                                       int32_t _min_occ_cutoff,
+                                                       bool _index_only_fwd_strand,
+                                                       int64_t _min_tlen) {
+    return std::shared_ptr<mindex::IndexParams>(new mindex::IndexParams(
+        _k, _w, _hp_supp, _max_hp_len, _freq_percentile, _min_occ_cutoff, _index_only_fwd_strand, _min_tlen));
+}
+
 
 }  // namespace mindex
 
