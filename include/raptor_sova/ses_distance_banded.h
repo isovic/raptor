d6f4ebc0085644209c0bc9e331585426579c6592
Ivan Sovic <ivan.sovic@gmail.com>
2019-11-22 18:01:26 +0100

    Implemented a modified ses distance calculation method which track scores.

--- include/raptor_sova/ses_distance_banded.h
+++ include/raptor_sova/ses_distance_banded.h
@@ -11,12 +11,25 @@ namespace ses {
 
 const int32_t MINUS_INF = std::numeric_limits<int32_t>::min() + 10000;  // We need a margin to avoid overflows.
 
+class SesResults {
+  public:
+    int32_t last_q = 0;
+    int32_t last_t = 0;
+    int32_t last_score = MINUS_INF;
+    int32_t max_q = 0;
+    int32_t max_t = 0;
+    int32_t max_score = MINUS_INF;
+    int32_t diffs = 0;
+    bool valid = false;
+};
+
 /*
  * These routines implement a banded diff calculation.
  * They are fast, but do not offer traceback.
 */
 int32_t BandedSESDistance(const std::string& q, const std::string& t, double maxd_frac, double bandw_frac);
 int32_t BandedSESDistance(const char* q, size_t qlen, const char* t, size_t tlen, double maxd_frac, double bandw_frac);
+SesResults BandedSESDistanceAdvanced(const char* q, size_t qlen, const char* t, size_t tlen, double maxd_frac, double bandw_frac);
 
 }
 }
--- src/raptor_sova/ses_distance_banded.cc
+++ src/raptor_sova/ses_distance_banded.cc
@@ -81,5 +81,109 @@ int32_t BandedSESDistance(const char* q, size_t qlen, const char* t, size_t tlen
     return -1;
 }
 
+SesResults BandedSESDistanceAdvanced(const char* q, size_t qlen, const char* t, size_t tlen, double maxd_frac, double bandw_frac) {
+    SesResults ret;
+
+    const int32_t match_score = 1;
+    const int32_t indel_penalty = -1;
+
+    int32_t N = qlen;
+    int32_t M = tlen;
+    int32_t d_max = std::min(N + M, static_cast<int32_t>(maxd_frac * (N + M)));
+    int32_t band_w = qlen * bandw_frac;
+    int32_t score = 0;
+
+    int32_t zero_offset = d_max + 1;
+    std::vector<int32_t> v(2 * d_max + 3, MINUS_INF);
+
+    { // Initialization is required. Outside of the main loop, so that the
+        // MINUS_INF trick can be used.
+        int32_t d = 0, k = 0, x = 0, y = 0;
+        while (x < N && y < M && q[x] == t[y]) {
+            ++x;
+            ++y;
+        }
+        v[zero_offset] = x;
+
+        ret.max_score = x * match_score;
+        ret.max_q = x;
+        ret.max_t = y;
+        ret.diffs = 0;
+
+        if (x >= N || y >= M) {
+            ret.valid = true;
+            ret.last_score = ret.max_score;
+            ret.last_q = x;
+            ret.last_t = y;
+            return ret;
+        }
+    }
+
+    std::vector<int32_t> u(2 * d_max + 3, MINUS_INF);
+    std::vector<int32_t> scores(2 * d_max + 3, 0);
+
+    int32_t band_tolerance = band_w / 2 + 1;
+    int32_t min_k = -1, max_k = 1;  // +- 1 because we handled the '0' case above.
+    int32_t best_u = 0;
+
+    scores[zero_offset] = ret.max_score;
+
+    for (int32_t d = 1; d < d_max; d++) {
+        ret.diffs = d;
+        if ((max_k - min_k) > band_w) {
+            ret.valid = false;
+            break;
+        }
+
+        for (int32_t k = min_k; k <= max_k; k += 2) {
+            int32_t kz = k + zero_offset;
+            int32_t x = std::max(v[kz + 1], v[kz - 1] + 1);
+            int32_t y = x - k;
+            scores[kz] -= indel_penalty;
+            while (x < N && y < M && q[x] == t[y]) {
+                ++x;
+                ++y;
+                scores[kz] += match_score;
+            }
+
+            v[kz] = x;
+            u[kz] = x + y;
+            best_u = std::max(u[kz], best_u);
+
+            if (scores[kz] > ret.max_score) {
+                ret.max_score = scores[kz];
+                ret.max_q = x;
+                ret.max_t = y;
+            }
+
+            if (x >= N || y >= M) {
+                ret.valid = true;
+                ret.last_score = scores[kz];
+                ret.last_q = x;
+                ret.last_t = y;
+                break;
+            }
+        }
+
+        int32_t new_min_k = max_k;
+        int32_t new_max_k = min_k;
+        for (int32_t k = min_k; k <= max_k; k+=2) {
+            if (u[k + zero_offset] >= (best_u - band_tolerance)) {
+                new_min_k = std::min(k, new_min_k);
+                new_max_k = std::max(k, new_max_k);
+            }
+        }
+
+        if (ret.valid) {
+            break;
+        }
+
+        min_k = new_min_k - 1;
+        max_k = new_max_k + 1;
+    }
+
+    return ret;
+}
+
 }
 }
