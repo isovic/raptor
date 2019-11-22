bbd8c832259b4bf2771a2b82871ce6265faf721c
Ivan Sovic <ivan.sovic@gmail.com>
2019-11-22 13:15:38 +0100

    Refactoring of src/raptor_sova/sova_mapper.cc in progress. I separated diagonal anchor construction and alignment. The FormDiagonalAnchors now produces a vector of OverlapPtr objects, and they are converted to TargetHits later. Alignment is currently not applied.

--- src/raptor_sova/sova_mapper.cc
+++ src/raptor_sova/sova_mapper.cc
@@ -29,6 +29,8 @@
 #include <raptor_sova/seed_hit_diag.hpp>
 #include <raptor_sova/ses_distance_banded.h>
 
+#include <raptor_sova/sova_overlap.h>
+
 // #define USE_LIS_FILTER
 
 namespace raptor {
@@ -70,35 +72,31 @@ SovaMapper::~SovaMapper() {}
  * Forms anchors simply by binning seeds to narrow diagonals.
  * sorted_dh = sorted diagonal hits
 */
-std::vector<std::shared_ptr<raptor::TargetAnchorType>> FormDiagonalAnchors(
+std::vector<raptor::sova::OverlapPtr> FormDiagonalAnchors(
             const std::vector<mindex128_t>& sorted_dh,
             const mindex::SequencePtr& qseq,
             const mindex::IndexPtr& index,
-            int32_t chain_bandwidth, double align_bandwidth, double align_max_diff,
+            int32_t chain_bandwidth,
             int32_t min_num_seeds, int32_t min_span,
             bool overlap_skip_self_hits, bool overlap_single_arc) {
-    std::vector<std::shared_ptr<raptor::TargetAnchorType>> target_anchors;
+    std::vector<raptor::sova::OverlapPtr> overlaps;
 
     if (sorted_dh.empty()) {
-        return target_anchors;
+        return overlaps;
     }
 
     auto internal_dh = sorted_dh;
 
-    // Just a lookup to identify where to place an anchor.
-    std::unordered_map<int32_t, int32_t> tid_map;
-    int32_t num_anchors = 0;
-
     int32_t begin_id = 0;
     auto prev_shp = mindex::SeedHitDiagPacked(internal_dh[begin_id]);
     int32_t begin_diag = prev_shp.TargetPos() - prev_shp.QueryPos();
-    // int32_t begin_tid = prev_shp.TargetId();
-    // int32_t begin_trev = prev_shp.TargetRev();
-    // int32
+    int32_t min_tpos_id = 0, min_tpos = prev_shp.TargetPos();
+    int32_t max_tpos_id = 0, max_tpos = prev_shp.TargetPos();
 
     for (int32_t i = 0; i < static_cast<int32_t>(internal_dh.size()); ++i) {
         auto curr_shp = mindex::SeedHitDiagPacked(internal_dh[i]);
         int32_t curr_diag = curr_shp.TargetPos() - curr_shp.QueryPos();
+        auto curr_tpos = curr_shp.TargetPos();
         int32_t diag_diff = abs(curr_diag - begin_diag);
 
         if (curr_shp.TargetId() != prev_shp.TargetId() ||
@@ -111,6 +109,10 @@ std::vector<std::shared_ptr<raptor::TargetAnchorType>> FormDiagonalAnchors(
             kx::radix_sort(internal_dh.begin() + begin_id, internal_dh.begin() + i);
             auto begin_shp = mindex::SeedHitDiagPacked(internal_dh[begin_id]);
             auto end_shp = mindex::SeedHitDiagPacked(internal_dh[i-1]);
+
+            // auto begin_shp = mindex::SeedHitDiagPacked(internal_dh[min_tpos_id]);
+            // auto end_shp = mindex::SeedHitDiagPacked(internal_dh[max_tpos_id]);
+
             int32_t tid = begin_shp.TargetId();
             int32_t num_seeds = i - begin_id;
             int32_t qspan = end_shp.QueryPos() - begin_shp.QueryPos();
@@ -123,86 +125,192 @@ std::vector<std::shared_ptr<raptor::TargetAnchorType>> FormDiagonalAnchors(
                 continue;
             }
 
-            // if (num_seeds < min_num_seeds || qspan < min_span || tspan < min_span) {
-            //     continue;
-            // }
-
             if (num_seeds >= min_num_seeds && qspan > min_span && tspan > min_span &&
                     (overlap_skip_self_hits == false || (overlap_skip_self_hits && tid != qseq->abs_id())) &&
                     (overlap_single_arc == false || (overlap_single_arc && tid < qseq->abs_id()))) {
-                /////////////////////////
-                /// Add a new anchor. ///
-                /////////////////////////
-                // The t_id of a chain could have clashes with rev cmp. Encoding
-                // it again with the rev cmp flag will avoid this.
-                int32_t tkey = prev_shp.TargetIdRev() << 1;
-                auto it = tid_map.find(tkey);
-                if (it == tid_map.end()) {
-                    tid_map[tkey] = target_anchors.size();
-                    auto new_env = raptor::createMappingEnv(
-                            tid, 0, index->len(tid), begin_shp.TargetRev(),
-                            qseq->abs_id(), qseq->len(), false);
-                    auto anchor = std::shared_ptr<raptor::TargetAnchorType>(
-                        new raptor::TargetAnchorType(new_env));
-                    target_anchors.emplace_back(anchor);
-                    it = tid_map.find(tkey);
-                }
-                auto& tanchors_ref = target_anchors[it->second];
-                int32_t cov_bases_q = 0, cov_bases_t = 0;
-                int32_t edit_dist = -1, score = 0;
+                //////////////////////////
+                /// Add a new overlap. ///
+                //////////////////////////
+                float score = num_seeds;
+                float idt = 0.0;
+                int32_t edit_dist = -1;
+                auto new_ovl = raptor::sova::createOverlap(
+                    qseq->header(), index->header(tid), score, idt,
+                    0, begin_shp.QueryPos(), end_shp.QueryPos(), qseq->len(),
+                    begin_shp.TargetRev(), begin_shp.TargetPos(), end_shp.TargetPos(), index->len(tid),
+                    "local", qseq->abs_id(), tid, edit_dist, num_seeds
+                );
+                overlaps.emplace_back(std::move(new_ovl));
+            }
+            min_tpos = max_tpos = curr_tpos;
+            min_tpos_id = max_tpos_id = i;
+        }
+        // Track the minimum and maximum target positions for each diagonal.
+        if (curr_tpos < min_tpos) {
+            min_tpos_id = i;
+            min_tpos = curr_tpos;
+        }
+        if (curr_tpos > max_tpos) {
+            max_tpos_id = i;
+            max_tpos = curr_tpos;
+        }
 
-                int32_t tstart = tanchors_ref->env()->t_rev ? (tanchors_ref->env()->t_len - end_shp.TargetPos()) : begin_shp.TargetPos();
-                int32_t tend = tanchors_ref->env()->t_rev ? (tanchors_ref->env()->t_len - begin_shp.TargetPos()) : end_shp.TargetPos();
+        prev_shp = curr_shp;
+    }
 
-                auto tseq = index->FetchSeqAsString(tanchors_ref->env()->t_id, tstart, tend, tanchors_ref->env()->t_rev);
-                auto qseq_str = qseq->GetSubsequenceAsString(begin_shp.QueryPos(), end_shp.QueryPos());
-                int32_t qspan = end_shp.QueryPos() - begin_shp.QueryPos();
-                int32_t tspan = end_shp.TargetPos() - begin_shp.TargetPos();
-                // if (tanchors_ref->env()->t_rev) {
-                //     std::cerr << qseq_str << "\n" << tseq << "\n";
-                // }
-                int32_t num_diffs = -1;
-                num_diffs = raptor::ses::BandedSESDistance(
+    return overlaps;
+}
+
+/*
+ * This function will take an overlap, and perform banded global alignment
+ * of the overlap from it's start to end.
+ * The produced overlap will have the same start/end coordinates, and only
+ * different scores/edit distance.
+*/
+raptor::sova::OverlapPtr AlignOverlap(
+                const mindex::IndexPtr& index,
+                const mindex::SequencePtr& qseq,
+                const raptor::sova::OverlapPtr& ovl,
+                double align_bandwidth, double align_max_diff) {
+    if (ovl == nullptr) {
+        return nullptr;
+    }
+    raptor::sova::OverlapPtr ret = raptor::sova::createOverlap(ovl);
+
+    int32_t edit_dist = -1, score = 0;
+
+    int32_t tstart = ovl->b_rev ? (ovl->b_len - ovl->b_end) : ovl->b_start;
+    int32_t tend = ovl->b_rev ? (ovl->b_len - ovl->b_start) : ovl->b_end;
+
+    auto tseq = index->FetchSeqAsString(ovl->b_id, tstart, tend, ovl->b_rev);
+    auto qseq_str = qseq->GetSubsequenceAsString(ovl->a_start, ovl->a_end);
+    int32_t qspan = ovl->a_end - ovl->a_start;
+    int32_t tspan = ovl->b_end - ovl->b_start;
+    int32_t num_diffs = raptor::ses::BandedSESDistance(
                             qseq_str.c_str(),
                             qspan,
                             tseq.c_str(),
                             tspan,
                             align_max_diff,
                             align_bandwidth);
-                edit_dist = num_diffs;
-                score = num_seeds;
+    ret->edit_dist = num_diffs;
+    ret->score = ret->num_seeds;
 
-                tanchors_ref->hits().emplace_back(
-                    raptor::createRegionMapped(
-                                num_anchors, -1, tanchors_ref->env(),
-                                begin_shp.QueryPos(), end_shp.QueryPos(),
-                                begin_shp.TargetPos(), end_shp.TargetPos(),
-                                cov_bases_q, cov_bases_t, num_seeds,
-                                edit_dist, score, -1, -1, -1, -1));
+    return ret;
+}
 
-                ++num_anchors;
-            }
-        }
+/*
+ * ExtendAlignOverlap will take an overlap and attempt to align it beyond
+ * the start and end positions, "extending" it on both ends.
+ * The new produced overlap will have modified start/end coordinates.
+*/
+raptor::sova::OverlapPtr ExtendAlignOverlap(
+                const mindex::IndexPtr& index,
+                const mindex::SequencePtr& qseq,
+                const raptor::sova::OverlapPtr& ovl,
+                double align_bandwidth, double align_max_diff) {
+    if (ovl == nullptr) {
+        return nullptr;
+    }
+    raptor::sova::OverlapPtr ret = raptor::sova::createOverlap(ovl);
 
-        // Set the same diagonal value to any seed within the bandwidth.
-        // This will enable sorting by TargetPos.
-        mindex::SeedHitDiagPacked::EncodeDiagonal(internal_dh[i], begin_diag);
-        prev_shp = curr_shp;
+    int32_t edit_dist = -1, score = 0;
+
+    int32_t tstart = ovl->b_rev ? (ovl->b_len - ovl->b_end) : ovl->b_start;
+    int32_t tend = ovl->b_rev ? (ovl->b_len - ovl->b_start) : ovl->b_end;
+
+    auto tseq = index->FetchSeqAsString(ovl->b_id, tstart, tend, ovl->b_rev);
+    auto qseq_str = qseq->GetSubsequenceAsString(ovl->a_start, ovl->a_end);
+    int32_t qspan = ovl->a_end - ovl->a_start;
+    int32_t tspan = ovl->b_end - ovl->b_start;
+    int32_t num_diffs = raptor::ses::BandedSESDistance(
+                            qseq_str.c_str(),
+                            qspan,
+                            tseq.c_str(),
+                            tspan,
+                            align_max_diff,
+                            align_bandwidth);
+    ret->edit_dist = num_diffs;
+    ret->score = ret->num_seeds;
+
+    return ret;
+}
+
+std::vector<std::shared_ptr<raptor::TargetAnchorType>> OverlapsToTargetAnchors(const std::vector<raptor::sova::OverlapPtr>& overlaps) {
+    std::vector<std::shared_ptr<raptor::TargetAnchorType>> ta;
+
+    if (overlaps.empty()) {
+        return ta;
     }
 
-    return target_anchors;
+    std::unordered_map<int32_t, int32_t> tid_map;
+    for (int32_t i = 0, curr_ta = -1; i < static_cast<int32_t>(overlaps.size()); ++i) {
+        const auto& curr = overlaps[i];
+        if (i == 0 || overlaps[i]->b_id != overlaps[i-1]->b_id ||
+                        overlaps[i]->b_rev != overlaps[i-1]->b_rev) {
+            // Create a new target anchor.
+            // The t_id of a chain could have clashes with rev cmp. Encoding
+            // it again with the rev cmp flag will avoid this.
+            // const auto& prev = (i > 0) ? overlaps[i-1] : overlaps[i];
+            int32_t tkey = (curr->b_id << 1) | static_cast<int32_t>(curr->b_rev);
+            auto it = tid_map.find(tkey);
+            if (it == tid_map.end()) {
+                tid_map[tkey] = ta.size();
+                auto new_env = raptor::createMappingEnv(
+                        curr->b_id, 0, curr->b_len, curr->b_rev,
+                        curr->a_id, curr->a_len, curr->a_rev);
+                auto new_ta_container = std::shared_ptr<raptor::TargetAnchorType>(
+                        new raptor::TargetAnchorType(new_env));
+                ta.emplace_back(new_ta_container);
+                it = tid_map.find(tkey);
+                curr_ta = it->second;
+            }
+        }
+        auto& ta_container = ta[curr_ta];
+
+        int32_t cov_bases_q = curr->a_end - curr->a_start;
+        int32_t cov_bases_t = curr->b_end - curr->b_start;
+        ta_container->hits().emplace_back(
+            raptor::createRegionMapped(
+                        i, -1, ta_container->env(),
+                        curr->a_start, curr->a_end,
+                        curr->b_start, curr->b_end,
+                        cov_bases_q, cov_bases_t,
+                            curr->num_seeds, curr->edit_dist, curr->score,
+                        -1, -1, -1, -1));
+    }
+
+    return ta;
 }
 
-std::vector<std::shared_ptr<raptor::TargetAnchorType>> AlignAnchors(
-            const std::vector<std::shared_ptr<raptor::TargetAnchorType>> ta,
-            const mindex::SequencePtr& qseq,
-            const mindex::IndexPtr& index,
-            int32_t bandwidth, int32_t min_num_seeds, int32_t min_span,
-            bool overlap_skip_self_hits, bool overlap_single_arc) {
+// std::vector<std::shared_ptr<raptor::TargetAnchorType>> AlignAnchors(
+//             const std::vector<std::shared_ptr<raptor::TargetAnchorType>> ta,
+//             const mindex::SequencePtr& qseq,
+//             const mindex::IndexPtr& index,
+//             int32_t bandwidth, int32_t min_num_seeds, int32_t min_span,
+//             bool overlap_skip_self_hits, bool overlap_single_arc) {
 
-    std::vector<std::shared_ptr<raptor::TargetAnchorType>> aligned_ta;
-    return aligned_ta;
-}
+//     std::vector<std::shared_ptr<raptor::TargetAnchorType>> aligned_ta;
+
+//     for (auto& one_ta: ta) {
+// 		if (one_ta == nullptr) {
+// 			continue;
+// 		}
+
+// 		// const std::vector<std::shared_ptr<raptor::RegionBase>>& regions_to_write = result->regions();
+
+
+//         // for (size_t i = 0; i < regions_to_write.size(); i++) {
+//         //     auto aln = regions_to_write[i];
+//         //     bool is_secondary = (aln->PathId() > 0);
+//         //     bool is_supplementary = (aln->SegmentId() > 0);
+//         //     auto& qseq = seqs->GetSeqByAbsID(aln->QueryID());
+// 		// 	PrintAsM4(stdout, index, qseq, aln);
+//         // }
+//     }
+
+//     return aligned_ta;
+// }
 
 // std::cerr << PrintInt128(mindex::MASK_INV_SEED_HIT_DIAG_0) << "\n";
 // std::cerr << PrintInt128(mindex::MASK_INV_SEED_HIT_DIAG_1) << "\n";
@@ -278,12 +386,13 @@ std::shared_ptr<raptor::LinearMappingResult> raptor::sova::SovaMapper::Map(const
     tt_sort.stop();
 
     TicToc tt_chain;
-    std::vector<std::shared_ptr<raptor::TargetAnchorType>> anchors =
-                        FormDiagonalAnchors(seed_hits_diag, qseq, index_,
-                                params_->chain_bandwidth, params_->align_bandwidth, params_->align_max_diff,
-                                params_->min_num_seeds, params_->chain_min_span,
-                                params_->ref_and_reads_path_same && params_->overlap_skip_self_hits,
-                                params_->ref_and_reads_path_same && params_->overlap_single_arc);
+    auto overlaps = FormDiagonalAnchors(seed_hits_diag, qseq, index_,
+                        params_->chain_bandwidth,
+                        params_->min_num_seeds, params_->chain_min_span,
+                        params_->ref_and_reads_path_same && params_->overlap_skip_self_hits,
+                        params_->ref_and_reads_path_same && params_->overlap_single_arc);
+    auto anchors = OverlapsToTargetAnchors(overlaps);
+    // std::vector<std::shared_ptr<raptor::TargetAnchorType>> anchors;
     tt_chain.stop();
 
     result->timings()["map_hits_collect"] = tt_collect_hits.get_microsecs();
